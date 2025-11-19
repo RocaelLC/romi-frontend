3d83163e57e6e13cf52d8c2a21f031e55b8e860b
RocaelLC <rocaellc42@gmail.com>
2025-11-18 19:42:25 -0600

    update

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -96,14 +96,7 @@ export default function Page() {
           >
             Explorar Servicio
           </Link>
-          <Link
-            href="/appointments"
-            className="mt-4 inline-flex items-center justify-center
-                       rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium
-                       text-foreground shadow-sm "
-          >
-            Mis citas mientras
-          </Link>
+          
 
         </div>
       </section>
